ce96c7e47a961173b78d46ab4997d5e832946466
Bryant <bryantwiltshire@gmail.com>
2026-03-07 20:53:09 +0000

    Fix missing signIn import in register.tsx

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
+import { signIn } from 'next-auth/react'
 
 export default function Register() {
   const [username, setUsername] = useState('')
